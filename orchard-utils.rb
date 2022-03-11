4e96b244677fd4a05d1ec08da6c7ea008d06c93a
Jay Fidlow <jfidlow@theorchard.com>
2022-03-11 15:13:42 -0500

    update deps

--- orchard-utils.rb
+++ orchard-utils.rb
@@ -5,8 +5,23 @@ class OrchardUtils < Formula
     sha256 "FILL-THIS-IN"
     license "MIT"
 
+    depends_on "aws"
+
     def install
         bin.install 'aws-creds-generator/aws-session-login'
         bin.install 'ecr-login/ecr-login'
+    end
+
+    test do
+        # `test do` will create, run in and delete a temporary directory.
+        #
+        # This test will fail and we won't accept that! For Homebrew/homebrew-core
+        # this will need to be a test that verifies the functionality of the
+        # software. Run the test with `brew test orchard-utils`. Options passed
+        # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+        #
+        # The installed folder is not in the path, so use the entire path to any
+        # executables being tested: `system "#{bin}/program", "do", "something"`.
+        system "#{bin}/ecr-login.sh"
     end
 end
